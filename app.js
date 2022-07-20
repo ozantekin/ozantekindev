9eb6ada94b6a608fdd7dc911ce5fc0b57fe4e700
Ozan Tekin <ardilozantekin@gmail.com>
2022-07-20 16:48:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const theme = document.getElementById('theme')
 
-theme.onclick = () => {
+theme.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme')
 
   if (document.body.classList.contains('dark-theme')) {
@@ -8,10 +8,10 @@ theme.onclick = () => {
   } else {
     theme.classList = 'fa-solid fa-cloud-rain fa-2x'
   }
-}
 
-localStorage.setItem('theme', document.body.classList)
-localStorage.setItem('icon', theme.classList)
+  localStorage.setItem('theme', document.body.classList)
+  localStorage.setItem('icon', theme.classList)
+})
 
 if (localStorage.getItem('theme') != '') {
   document.body.classList.add(localStorage.getItem('theme'))
